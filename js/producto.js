675fc53acce2206c88341ebf153397b67e87153e
LucasFerruchi <lucasferruchi05@gmail.com>
2023-07-09 12:23:50 -0300

    termino pagina producto

--- js/producto.js
+++ js/producto.js
@@ -18,7 +18,33 @@ const traerDatosProductos = () => {
   //2.c.iv-Encontrar el producto
   //!Ir primero a app.js a listarProducto
   let producto = productos.find((item) => item.id == idProducto);
-  //   console.log(producto);
+  console.log(producto); //colocar funcion abajo para ejecutar automatica
 
   //2.c.v-Crear la tarjeta (si existe)
+  if (producto) {
+    let col = document.createElement("div");
+    col.classList = "col";
+    let tarjeta = `<div class="card mb-3">
+    <div class="row g-0">
+    <div class="col-md-4">
+    <img src="${producto.image}" class="img-fluid rounded-start p-3" alt="${producto.title}">
+    </div>
+    <div class="col-md-8">
+    <div class="card-body">
+    <h5 class="card-title">${producto.title}</h5>
+    <p class="card-text">${producto.description}</p>
+    <p class="card-text"><small class="text-muted">Precio: $${producto.price}</small></p>
+    </div>
+    </div>
+    </div>
+    </div>`;
+
+    col.innerHTML = tarjeta;
+    contenedor.append(col);
+  } else {
+    console.log("No existe un producto con el id recibido");
+  }
+
+  //Si no existe
 };
+traerDatosProductos();
